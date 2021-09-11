369eb78283344375640919703b8fdc30ca9fa62b
p0ny <907577610@qq.com>
2021-09-10 22:27:25 -0700

    pass lab2 all tests

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -1,5 +1,7 @@
 #include "tcp_receiver.hh"
 
+#include <iostream>
+
 // Dummy implementation of a TCP receiver
 
 // For Lab 2, please replace with a real implementation that passes the
@@ -11,9 +13,25 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 using namespace std;
 
 void TCPReceiver::segment_received(const TCPSegment &seg) {
-    DUMMY_CODE(seg);
+    bool first = false;
+    if (!_isn.has_value() && seg.header().syn) {
+        _isn = seg.header().seqno;
+        first = true;
+    }
+    if (!_isn.has_value())
+        return;
+    uint64_t index = unwrap(seg.header().seqno + first, _isn.value(), _reassembler.stream_out().bytes_written());
+    _reassembler.push_substring(seg.payload().copy(), index - 1, seg.header().fin);
 }
 
-optional<WrappingInt32> TCPReceiver::ackno() const { return {}; }
+optional<WrappingInt32> TCPReceiver::ackno() const {
+    if (_isn.has_value()) {
+        auto n = _checkpoint() + 1;
+        if (_reassembler.stream_out().input_ended())
+            n += 1;
+        return wrap(n, _isn.value());
+    }
+    return {};
+}
 
-size_t TCPReceiver::window_size() const { return {}; }
+size_t TCPReceiver::window_size() const { return _capacity - _reassembler.stream_out().buffer_size(); }
